401ddc2b268e3b8311e3ce32e11daf979a41fcca
Redstoneur <alfa.aog36@gmail.com>
2026-02-16 23:33:20 +0100

    Update package API to expose key components and improve module accessibility

--- pymacrorecorder/__init__.py
+++ pymacrorecorder/__init__.py
@@ -1,7 +1,24 @@
-"""PyMacroRecorder package initialization."""
+"""PyMacroRecorder package public API."""
 
 from .app import App
+from .config import DEFAULT_HOTKEYS, load_config, save_config
+from .hotkeys import HotkeyManager, capture_hotkey_blocking
+from .models import Macro, MacroEvent
+from .player import Player
+from .recorder import Recorder
+from .storage import load_macros_from_csv, save_macro_to_csv
 
 __all__ = [
     "App",
+    "Recorder",
+    "Player",
+    "Macro",
+    "MacroEvent",
+    "HotkeyManager",
+    "capture_hotkey_blocking",
+    "DEFAULT_HOTKEYS",
+    "load_config",
+    "save_config",
+    "load_macros_from_csv",
+    "save_macro_to_csv",
 ]
